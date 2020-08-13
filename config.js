028e3f7f22a869a1f6a640d6753bd1a3eb6bfbba
Unknown <ariki1068@gmail.com>
2020-08-14 08:11:48 +0900

    公開URLを変更

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  url: 'https://ariki4160.github.io/',
+  url: 'https://ariki4160.github.io/vivo-blog',
   pathPrefix: '/',
   title: 'Blog by Masayuki Ariki',
   subtitle: 'ディープラーニングやサイト・データベース制作時の備忘録をGatsbyで、のーんびりと記録しています。',
